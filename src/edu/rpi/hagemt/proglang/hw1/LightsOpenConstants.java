cbec76b7e599f97864b352abd5be50deba8daf35
Tor E Hagemann <hagemt@rpi.edu>
2012-09-15 19:13:09 -0500

    Transitioned old project to proper scheme

---
+++ src/edu/rpi/hagemt/proglang/hw1/LightsOpenConstants.java
@@ -0,0 +1,11 @@
+/**
+ * LightsOpenConstants.java, part of Lights Open
+ * Author: Tor E Hagemann <hagemt@rpi.edu>
+ */
+package edu.rpi.hagemt.proglang.hw1;
+
+interface LightsOpenConstants extends javax.swing.SwingContants {
+  public final static int PADDING = 5;
+  public final static int CELL_SIZE = 192;
+  public final static char ACTIVE_STATE = 'W';
+}
